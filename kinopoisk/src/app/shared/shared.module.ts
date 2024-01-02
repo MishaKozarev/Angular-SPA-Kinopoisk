530bd977160b7a6f21b9fbb712a38dc7cb25e073
MishaKozarev <mishaaaaa1986@mail.ru>
2024-01-02 18:29:45 +0300

    feat: add material UI

--- kinopoisk/src/app/shared/shared.module.ts
+++ kinopoisk/src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FilmItemComponent } from './components/film-item/film-item.component';
 import { ColorRatingDirective } from './directives/color-rating.directive';
+import { MaterialModule } from './material/material.module';
 
 
 
@@ -12,10 +13,12 @@ import { ColorRatingDirective } from './directives/color-rating.directive';
   ],
   imports: [
     CommonModule,
+    MaterialModule,
   ],
   exports: [
     FilmItemComponent,
     ColorRatingDirective,
+    MaterialModule,
   ],
 })
 export class SharedModule { }
