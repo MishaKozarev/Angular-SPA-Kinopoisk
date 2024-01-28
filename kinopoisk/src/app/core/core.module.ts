45623e657918d4c5725aa480e9efeed5feb194bb
MishaKozarev <mishaaaaa1986@mail.ru>
2024-01-28 18:36:08 +0300

    feat: add elements in header

--- kinopoisk/src/app/core/core.module.ts
+++ kinopoisk/src/app/core/core.module.ts
@@ -4,10 +4,16 @@ import { HeaderComponent } from './components/header/header.component';
 import { SharedModule } from '../shared/shared.module';
 import { HttpClientModule } from '@angular/common/http';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { LogoHeaderComponent } from './components/logo-header/logo-header.component';
+import { FormHeaderComponent } from './components/form-header/form-header.component';
+import { IconSearchComponent } from './components/icon-search/icon-search.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
+    LogoHeaderComponent,
+    FormHeaderComponent,
+    IconSearchComponent,
     NotFoundPageComponent,
   ],
   imports: [
